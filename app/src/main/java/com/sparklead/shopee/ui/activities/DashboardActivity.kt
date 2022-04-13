c0f2d978f269ca1801a9f1e48d28f0c6cb052c20
Aditya Gupta <ashishgupta9936ad@gmail.com>
2022-04-13 17:20:44 +0530

    navigation bar edit

--- app/src/main/java/com/sparklead/shopee/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/sparklead/shopee/ui/activities/DashboardActivity.kt
@@ -1,13 +1,15 @@
 package com.sparklead.shopee.ui.activities
 
+import android.app.StatusBarManager
+import android.graphics.Color
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.sparklead.shopee.R
-//import com.sparklead.shopee.Activies.databinding.ActivityDashboardBinding
 
 class DashboardActivity : BaseActivity() {
 
@@ -15,6 +17,11 @@ class DashboardActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+
+        supportActionBar!!.setBackgroundDrawable(
+            ContextCompat.getDrawable(this,R.color.neww)
+        )
 
         val navController = findNavController(R.id.nav_host_fragment_activity_dashboard)
         // Passing each menu ID as a set of Ids because each
--- app/src/main/java/com/sparklead/shopee/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/sparklead/shopee/ui/fragments/DashboardFragment.kt
@@ -1,17 +1,22 @@
 package com.sparklead.shopee.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.sparklead.shopee.R
 //import com.sparklead.shopee.Activies.databinding.FragmentDashboardBinding
 import com.sparklead.shopee.databinding.FragmentDashboardBinding
+import com.sparklead.shopee.ui.activities.SettingActivity
 
 class DashboardFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,5 +30,23 @@ class DashboardFragment : Fragment() {
         textView.text = "This is the dashboard Fragment"
 
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when(id){
+
+            R.id.action_settings ->{
+                startActivity(Intent(activity,SettingActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
